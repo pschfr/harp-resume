af5b72a06d5b45c8288c6742c6c0d54d17782104
Paul Schaefer <yeppaul2@gmail.com>
2017-08-21 23:02:24 -0400

    Initial commit!!

---
+++ README.md
@@ -0,0 +1,2 @@
+# [Résumé](https://paulmakesthe.net/resume/)
+This is my résumé! It's made into a website with [`harp`](https://github.com/sintaxi/harp), [Tachyons-Sass](https://github.com/tachyons-css/tachyons-sass), [Tachyons-Print](https://github.com/lowmess/tachyons-print), and some magic! ✨
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+	util = require('gulp-util'),
+	vftp = require('vinyl-ftp');
+
+
+gulp.task('default', function() {
+	var conn = vftp.create({
+		host: 'paulmakesthe.net',
+		user: 'username',
+		pass: 'password',
+		parallel: 8,
+		log: util.log
+	}),
+	globs = 'www/**';
+
+	return gulp.src(globs, { buffer: false })
+		  .pipe(conn.newer('/public_html/resume'))
+		  .pipe(conn.dest('/public_html/resume'));
+});
